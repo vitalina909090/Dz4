55c43fa997d5e7dc5c9520e8d2f919c6c41a9a73
vitalina909090 <legezavitalina06@gmail.com>
2024-09-23 01:43:08 +0300

    Готове дз

--- Dz4.cpp
+++ Dz4.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 #include <Windows.h>
+#include <vector>
 
 using namespace std;
 
@@ -9,7 +10,7 @@ class Date {
     uint16_t month;
     uint16_t year;
 public:
-    Date(uint16_t day1, uint16_t month1, uint16_t year1) : day{ day1 }, month{ month1 }, year{ year1 } {}
+    Date(uint16_t day1, uint16_t month1, uint16_t year1) : day(day1), month(month1), year(year1) {}
     Date() : Date(0, 0, 0) {}
 
     void printDate() const {
@@ -26,55 +27,44 @@ class People {
     static int count;
 public:
     People(int IDP, const string& surnameP, const string& nameP, const string& patronymicP, Date dataP)
-        : ID{ IDP }, surname{ surnameP }, name{ nameP }, patronymic{ patronymicP }, data{ dataP } {
+        : ID(IDP), surname(surnameP), name(nameP), patronymic(patronymicP), data(dataP) {
         count++;
     }
 
-    People() : People(0, "", "", "", Date{ 0, 0, 0 }) {}
+    People() : People(0, "", "", "", Date(0, 0, 0)) {}
+
+    People(const People& other)
+        : ID(other.ID), surname(other.surname), name(other.name), patronymic(other.patronymic), data(other.data) {
+        count++;
+    }
 
     ~People() {
         count--;
     }
 
-    People(const People& other)
-        : ID{ other.ID }, surname{ other.surname }, name{ other.name }, patronymic{ other.patronymic }, data{ other.data } {
-        count++;
-    }
-
-    People& operator=(const People& other) {
-        if (this != &other) {
-            ID = other.ID;
-            surname = other.surname;
-            name = other.name;
-            patronymic = other.patronymic;
-            data = other.data;
-        }
-        return *this;
-    }
-
     void inputPerson() {
-        cout << "Введите ID: ";
+        cout << "Введіть ID: ";
         cin >> ID;
 
-        cin.ignore(); // Очистка буфера перед вводом строки
+        cin.ignore();
 
-        cout << "Введите фамилию: ";
+        cout << "Введіть прізвище: ";
         getline(cin, surname);
 
-        cout << "Введите имя: ";
+        cout << "Введіть ім'я: ";
         getline(cin, name);
 
-        cout << "Введите отчество: ";
+        cout << "Введіть по батькові: ";
         getline(cin, patronymic);
 
         int day, month, year;
-        cout << "Введите день рождения (дд мм гггг): ";
+        cout << "Введіть день народження (дд мм рррр): ";
         cin >> day >> month >> year;
         data = Date(day, month, year);
     }
 
     void output() const {
-        cout << "ID: " << ID << "\nФамилия: " << surname << "\nИмя: " << name << "\nОтчество: " << patronymic << "\nДата рождения: ";
+        cout << "ID: " << ID << "\nПрізвище: " << surname << "\nІм'я: " << name << "\nПо батькові: " << patronymic << "\nДата народження: ";
         data.printDate();
         cout << endl;
     }
@@ -88,85 +78,55 @@ class Apartment {
     int rooms;
     double area;
     int floor;
-    People* tenants;
-    int tenantCount;
-    int maxTenants;
+    vector<People> humans;
+
 public:
-    Apartment() : rooms{ 0 }, area{ 0 }, floor{ 0 }, tenants{ nullptr }, tenantCount{ 0 }, maxTenants{ 0 } {}
+    Apartment() : rooms(0), area(0), floor(0) {}
 
-    Apartment(int roomsP, double areaP, int floorP, int maxTenantsP)
-        : rooms{ roomsP }, area{ areaP }, floor{ floorP }, maxTenants{ maxTenantsP }, tenantCount{ 0 } {
-        tenants = new People[maxTenants];
-    }
-
-    ~Apartment() {
-        delete[] tenants;
-    }
+    Apartment(int roomsP, double areaP, int floorP)
+        : rooms(roomsP), area(areaP), floor(floorP) {}
 
     Apartment(const Apartment& other)
-        : rooms{ other.rooms }, area{ other.area }, floor{ other.floor }, tenantCount{ other.tenantCount }, maxTenants{ other.maxTenants } {
-        tenants = new People[maxTenants];
-        for (int i = 0; i < tenantCount; ++i) {
-            tenants[i] = other.tenants[i];
-        }
-    }
+        : rooms(other.rooms), area(other.area), floor(other.floor), humans(other.humans) {}
 
-    Apartment& operator=(const Apartment& other) {
-        if (this != &other) {
-            delete[] tenants;
-            rooms = other.rooms;
-            area = other.area;
-            floor = other.floor;
-            tenantCount = other.tenantCount;
-            maxTenants = other.maxTenants;
-            tenants = new People[maxTenants];
-            for (int i = 0; i < tenantCount; ++i) {
-                tenants[i] = other.tenants[i];
-            }
-        }
-        return *this;
-    }
+    ~Apartment() {}
 
-    void addTenant(const People& person) {
-        if (tenantCount < maxTenants) {
-            tenants[tenantCount++] = person;
-        }
-        else {
-            cout << "Квартира полна, нельзя добавить больше жильцов.\n";
-        }
+    void newPerson(const People& person) {
+        humans.push_back(person);
     }
 
     void output() const {
-        cout << "Квартира: " << rooms << " комнаты, площадь: " << area << " кв.м, этаж: " << floor << ".\n";
-        cout << "Количество жильцов: " << tenantCount << endl;
-        for (int i = 0; i < tenantCount; ++i) {
-            tenants[i].output();
+        cout << "Квартира: " << rooms << " кімнати, площа: " << area << " кв.м, поверх: " << floor << ".\n";
+        cout << "Кількість жильців: " << humans.size() << endl;
+        for (int i = 0; i < humans.size(); i++) {
+            humans[i].output();
         }
     }
 
-    int getTenantCount() const { return tenantCount; }
+    int getHumanCount() const { return humans.size(); }
     int getFloor() const { return floor; }
 
-    People* findTenantByID(int ID) const {
-        for (int i = 0; i < tenantCount; ++i) {
-            if (tenants[i].getID() == ID) {
-                return &tenants[i];
+    bool findHumanByID(int ID, People& foundHuman) {
+        for (int i = 0; i < humans.size(); i++) {
+            if (humans[i].getID() == ID) {
+                foundHuman = humans[i];
+                return true;
             }
         }
-        return nullptr;
+        return false;
     }
 
-    void removeTenantByID(int ID) {
-        for (int i = 0; i < tenantCount; ++i) {
-            if (tenants[i].getID() == ID) {
-                for (int j = i; j < tenantCount - 1; ++j) {
-                    tenants[j] = tenants[j + 1];
-                }
-                tenantCount--;
-                return;
+    bool deleteHumanByID(int ID) {
+        for (int i = 0; i < humans.size(); i++) {
+            if (humans[i].getID() == ID) {
+                humans[i] = humans.back();
+                humans.pop_back();
+                cout << "Жильця з ID " << ID << " видалено.\n";
+                return true;
             }
         }
-        cout << "Жилец с ID " << ID << " не найден.\n";
+        cout << "Жильця з ID " << ID << " не знайдено.\n";
+        return false;
     }
 };
 
@@ -174,215 +134,138 @@ class House {
     int floors;
     Apartment apartments[6];
 public:
-    House() : floors{ 3 } {
-        apartments[0] = Apartment(2, 60.0, 1, 3);
-        apartments[1] = Apartment(3, 75.0, 1, 2);
-        apartments[2] = Apartment(2, 65.0, 2, 2);
-        apartments[3] = Apartment(3, 80.0, 2, 3);
-        apartments[4] = Apartment(4, 90.0, 3, 4);
-        apartments[5] = Apartment(2, 70.0, 3, 1);
+    House() : floors(3) {
+        apartments[0] = Apartment(2, 60.0, 1);
+        apartments[1] = Apartment(3, 75.0, 1);
+        apartments[2] = Apartment(2, 65.0, 2);
+        apartments[3] = Apartment(3, 80.0, 2);
+        apartments[4] = Apartment(4, 90.0, 3);
+        apartments[5] = Apartment(2, 70.0, 3);
     }
 
-    House(const House& other) : floors{ other.floors } {
-        for (int i = 0; i < 6; ++i) {
+    House(const House& other) : floors(other.floors) {
+        for (int i = 0; i < 6; i++) {
             apartments[i] = other.apartments[i];
         }
     }
 
-    House& operator=(const House& other) {
-        if (this != &other) {
-            floors = other.floors;
-            for (int i = 0; i < 6; ++i) {
-                apartments[i] = other.apartments[i];
-            }
-        }
-        return *this;
-    }
+    ~House() {}
 
     void outputGeneralInfo() const {
-        cout << "Этажей в доме: " << floors << endl;
-        cout << "Квартир в доме: 6" << endl;
-        int totalTenants = 0;
-        for (const auto& apartment : apartments) {
-            totalTenants += apartment.getTenantCount();
+        cout << "Поверхів в будинку: " << floors << "\nКвартир в будинку: 6" << endl;
+        int totalHumans = 0;
+        for (int i = 0; i < 6; i++) {
+            totalHumans += apartments[i].getHumanCount();
         }
-        cout << "Всего жильцов в доме: " << totalTenants << endl;
+        cout << "Всього жильців в будинку: " << totalHumans << endl;
     }
 
-    void outputApartmentInfo(int index) const {
-        if (index >= 0 && index < 6) {
-            apartments[index].output();
+    void outputApartmentInfo(int num) const {
+        if (num >= 0 && num < 6) {
+            apartments[num].output();
         }
         else {
-            cout << "Некорректный номер квартиры.\n";
+            cout << "Некоректний номер квартири.\n";
         }
     }
 
-    People* findTenantByID(int ID) {
-        for (auto& apartment : apartments) {
-            People* foundTenant = apartment.findTenantByID(ID);
-            if (foundTenant != nullptr) {
-                cout << "Жилец с ID " << ID << " найден в квартире на " << apartment.getFloor() << " этаже.\n";
-                return foundTenant;
+    bool findHumanByID(int ID, People& foundHuman) {
+        for (int i = 0; i < 6; i++) {
+            if (apartments[i].findHumanByID(ID, foundHuman)) {
+                cout << "Жильця з ID " << ID << " знайдено в квартирі на " << apartments[i].getFloor() << " поверсі.\n";
+                return true;
             }
         }
-        return nullptr;
+        return false;
     }
 
-    void removeTenantByID(int ID) {
-        for (auto& apartment : apartments) {
-            apartment.removeTenantByID(ID);
+    void deleteHumanByID(int ID) {
+        bool found = false;
+        for (int i = 0; i < 6; i++) {
+            if (apartments[i].deleteHumanByID(ID)) {
+                found = true;
+            }
+        }
+        if (!found) {
+            cout << "Жильця з ID " << ID << " не знайдено в будинку.\n";
         }
     }
 
-    Apartment* getApartments() {
-        return apartments;
+    void newHumanToApartment(int num, const People& human) {
+        if (num >= 0 && num < 6) {
+            apartments[num].newPerson(human);
+            cout << "Жильця успішно додано до квартири.\n";
+        }
+        else {
+            cout << "Некоректний номер квартири.\n";
+        }
     }
 };
-
-void menu(House& house) {
-    int choice = 0;
-    do {
-        cout << "\n--- Главное меню ---\n";
-        cout << "1. Просмотреть информацию\n";
-        cout << "2. Изменить информацию\n";
-        cout << "3. Выйти\n";
-        cout << "Введите ваш выбор: ";
-
-        while (!(cin >> choice) || (choice < 1 || choice > 3)) {
-            cin.clear();
-            cin.ignore(10000, '\n');
-            cout << "Некорректный ввод. Пожалуйста, введите число от 1 до 3: ";
-        }
-
-        switch (choice) {
-        case 1: {
-            int subChoice;
-            cout << "\n--- Просмотреть информацию ---\n";
-            cout << "1.1 Общая информация о доме\n";
-            cout << "1.2 Информация о квартире и жителях\n";
-            cout << "1.3 Информация о жильце по ID\n";
-            cout << "Введите ваш выбор: ";
-
-            while (!(cin >> subChoice) || (subChoice < 1 || subChoice > 3)) {
-                cin.clear();
-                cin.ignore(10000, '\n');
-                cout << "Некорректный ввод. Пожалуйста, введите число от 1 до 3: ";
-            }
-
-            switch (subChoice) {
-            case 1:
-                house.outputGeneralInfo();
-                break;
-            case 2: {
-                int apartmentIndex;
-                cout << "Введите номер квартиры (1 - 6): ";
-
-                while (!(cin >> apartmentIndex) || (apartmentIndex < 1 || apartmentIndex > 6)) {
-                    cin.clear();
-                    cin.ignore(10000, '\n');
-                    cout << "Некорректный ввод. Пожалуйста, введите номер квартиры от 1 до 6: ";
-                }
-                apartmentIndex--; // переводим в индекс массива
-
-                house.outputApartmentInfo(apartmentIndex);
-                break;
-            }
-            case 3: {
-                int tenantID;
-                cout << "Введите ID жильца: ";
-
-                while (!(cin >> tenantID)) {
-                    cin.clear();
-                    cin.ignore(10000, '\n');
-                    cout << "Некорректный ввод. Пожалуйста, введите ID жильца: ";
-                }
-                People* tenant = house.findTenantByID(tenantID);
-                if (tenant) {
-                    tenant->output();
-                }
-                else {
-                    cout << "Жилец с ID " << tenantID << " не найден.\n";
-                }
-                break;
-            }
-            default:
-                cout << "Некорректный выбор.\n";
-                break;
-            }
-            break;
-        }
-        case 2: {
-            int subChoice;
-            cout << "\n--- Изменить информацию ---\n";
-            cout << "2.1 Добавить нового жильца\n";
-            cout << "2.2 Удалить жильца\n";
-            cout << "Введите ваш выбор: ";
-
-            while (!(cin >> subChoice) || (subChoice < 1 || subChoice > 2)) {
-                cin.clear();
-                cin.ignore(10000, '\n');
-                cout << "Некорректный ввод. Пожалуйста, введите число 1 или 2: ";
-            }
-
-            switch (subChoice) {
-            case 1: {
-                int apartmentIndex;
-                cout << "Введите номер квартиры (1 - 6): ";
-
-                while (!(cin >> apartmentIndex) || (apartmentIndex < 1 || apartmentIndex > 6)) {
-                    cin.clear();
-                    cin.ignore(10000, '\n');
-                    cout << "Некорректный ввод. Пожалуйста, введите номер квартиры от 1 до 6: ";
-                }
-                apartmentIndex--;
-
-                People newPerson;
-                newPerson.inputPerson(); // Ввод данных нового жильца
-
-                cout << "Информация о новом жильце:\n";
-                newPerson.output();
-
-                house.getApartments()[apartmentIndex].addTenant(newPerson);
-                break;
-            }
-            case 2: {
-                int tenantID;
-                cout << "Введите ID жильца для удаления: ";
-
-                while (!(cin >> tenantID)) {
-                    cin.clear();
-                    cin.ignore(10000, '\n');
-                    cout << "Некорректный ввод. Пожалуйста, введите ID жильца: ";
-                }
-                house.removeTenantByID(tenantID);
-                break;
-            }
-            default:
-                cout << "Некорректный выбор.\n";
-                break;
-            }
-            break;
-        }
-        case 3:
-            cout << "Выход из программы.\n";
-            break;
-        default:
-            cout << "Некорректный выбор.\n";
-            break;
-        }
-
-        // Очистка состояния ввода для предотвращения застревания
-        cin.clear();
-        cin.ignore(10000, '\n');
-
-    } while (choice != 3);
-}
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     House house;
-    menu(house);
+
+    int choice = 1;
+    do {
+        cout << "\n--- Головне меню ---\n" <<
+            "1. Показати інформацію про будинок\n" <<
+            "2. Показати інформацію про квартиру\n" <<
+            "3. Знайти жильця за ID\n" <<
+            "4. Видалити жильця за ID\n" <<
+            "5. Додати жильця в квартиру\n" <<
+            "0. Вийти\n" <<
+            "Оберіть дію: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            house.outputGeneralInfo();
+            break;
+        case 2: {
+            int num;
+            cout << "Введіть номер квартири (0-5): ";
+            cin >> num;
+            house.outputApartmentInfo(num);
+            break;
+        }
+        case 3: {
+            int ID;
+            cout << "Введіть ID жильця: ";
+            cin >> ID;
+            People foundHuman;
+            if (house.findHumanByID(ID, foundHuman)) {
+                foundHuman.output();
+            }
+            else {
+                cout << "Жильця з таким ID не знайдено\n";
+            }
+            break;
+        }
+        case 4: {
+            int ID;
+            cout << "Введіть ID жильця для видалення: ";
+            cin >> ID;
+            house.deleteHumanByID(ID);
+            break;
+        }
+        case 5: {
+            int apartmentNum;
+            cout << "Введіть номер квартири (0-5): ";
+            cin >> apartmentNum;
+            People newHuman;
+            newHuman.inputPerson();
+            house.newHumanToApartment(apartmentNum, newHuman);
+            break;
+        }
+        case 0:
+            cout << "Вихід\n";
+            break;
+        default:
+            cout << "Некоректний вибір\n";
+        }
+    } while (choice != 0);
+
     return 0;
 }
